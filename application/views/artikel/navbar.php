7e48653c6622501e2646ab128b2c1466d1750bff
sasmitaputrii <sasmitaputri706@gmail.com>
2024-01-08 12:35:51 +0800

    putri

--- application/views/artikel/navbar.php
+++ application/views/artikel/navbar.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-expand-lg sticky-top" style="background-color: #1F8A70">
-    <div class="container-fluid" style="background-color: #1F8A70;">
-        <a class="navbar-brand text-white" href="index.html">
-        <img src="<?=base_url()?>assets/img/logo.png"height="50" class="mr-2"> 
-        <strong>Nutri</strong>Track</a>
-    </div>
-    <div class="container" id="home">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link mr-4 text-light" href="#home">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mr-4 text-light" href="#artikel">ARTIKEL</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mr-4 text-light" href="#about">ABOUT</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mr-4 text-light" href="#benefit">BENEFIT</a>
-                </li>
-            </ul>
-            <div class="auth-buttons">
-                <button class="login-btn btn btn-primary" onclick="login()">LOGIN</button>
-        
-=======
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
     <div class="container-fluid d-flex align-items-center justify-content-between">
@@ -52,22 +22,6 @@
 
       <div class="auth-buttons">
                 <button class="login-btn btn btn-warning" onclick="login()">Login</button>
->>>>>>> 641e5f51c029f18b75dc7da3eb26a6004a16b420
             </div>
     </div>
-<<<<<<< HEAD
-</nav>
- <!-- Navbar End -->
-  <!-- Jumbotron -->
-  <div class="jumbotron jumbotron-fluid text-center" style="background-image: url(<?=base_url()?>assets/img/bg15.jpg)";>
-    <div class="container">
-        <h1 class="display-4 " style="color: black;"><span class="font-weight-bold">Selamat Datang</span></h1>
-        <hr>
-        <p class="lead font-weight-bold" style="color: black;" >Terima Kasih Telah Mengunjungi Catatan Nutrisi Kami<br>
-            Enjoy Your Meal</p>
-    </div>`
-</div>
-  <!-- Akhir Jumbotron -->
-=======
   </header><!-- End Header -->
->>>>>>> 641e5f51c029f18b75dc7da3eb26a6004a16b420
