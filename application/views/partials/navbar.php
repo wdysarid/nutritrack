b5bacd4f26597b6820952c1784e7f100db42860d
wdysarid <widyasaridamayanti@gmail.com>
2024-01-25 00:24:01 +0800

    pinish part 2

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -11,12 +11,6 @@
 <nav class="header-nav ms-auto">
   <ul class="d-flex align-items-center">
 
-    <li class="nav-item d-block d-lg-none">
-      <a class="nav-link nav-icon search-bar-toggle " href="#">
-        <i class="bi bi-search"></i>
-    </a>
-    </li><!-- End Search Icon-->
-
     <li class="nav-item dropdown pe-3">
       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
       <?php foreach($member as $key):?>
@@ -29,7 +23,7 @@
             <?php endif;?>
             <span class="d-none d-md-block dropdown-toggle ps-2 text-white"><?=$key['nama_lengkap']?></span>
 
-      </a><!-- End Profile Iamge Icon -->
+      </a><!-- End Profile Image Icon -->
 
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li>
